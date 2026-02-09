5cc3d1913966a0df183d09a3fb325fedeaaa5c19
Giorgio Marziani de Paolis <266694+netrace@users.noreply.github.com>
2026-02-09 21:05:15 +0100

    Add Heltec board pin configuration

---
+++ src/boards/heltec.rs
@@ -0,0 +1,18 @@
+// Heltec board pin configuration
+
+pub const HELTEC_PIN_CONFIG: [&str; 12] = [
+    "GPIO0",   // Pin for button
+    "GPIO1",   // Pin for LED
+    "GPIO2",   // Pin for I2C SDA
+    "GPIO3",   // Pin for I2C SCL
+    "GPIO4",   // Additional pin
+    "GPIO5",   // Additional pin
+    "GPIO12",  // Additional pin
+    "GPIO13",  // Additional pin
+    "GPIO14",  // Additional pin
+    "GPIO15",  // Additional pin
+    "GPIO16",  // Additional pin
+    "GPIO17",  // Additional pin
+];
+
+// Additional configurations specific to Heltec boards can be added here.
